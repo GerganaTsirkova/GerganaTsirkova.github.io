011e823e9146ab1bb5bf5095651c2a5e82319339
GerganaTsirkova <g_cirkova@mail.bg>
2021-03-28 19:49:21 +0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,59 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var concat      = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var express     = require('express');
+var app         = express();
+var server      = require('http').Server(app);
+var path        = require('path');
+var livereload  = require('gulp-livereload')
+var browserSync = require('browser-sync').create()
+var serverPort  = 8000;
 
-// complie scss in css
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: '.'
+    },
+  });
+});
 
-function style() {
-    //where is my sass file
-    return gulp.src('./scss/**/*.scss')
-        //pass it to sass compiler
-        .pipe(sass().on('error', sass.logError))
-        //where to coplile CSS
-        .pipe(gulp.dest('./css'))
-        //stream changes to all browserSync
-        .pipe(browserSync.stream());
-}
+gulp.task('sass', function() {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'expanded',
+            sourceComments: true,
+            errLogToConsole: false,
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            versions: ['last 2 browsers']
+        }))
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
 
-function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
+gulp.task('js', function() {
+  return gulp.src('/js/**/*.js')
+    .pipe( gulp.dest('/js/'))
+    .pipe(livereload());
+});
+
+gulp.task('express', function() {
+    app.use(express.static(path.resolve('./')));
+    server.listen(serverPort, function(){
+        console.log('Servidor corriendo en http://localhost:'+serverPort);
     });
-    gulp.watch('./scss/**/*.scss', style);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
-}
+});
 
-exports.style = style;
-exports.watch = watch;
+gulp.task('watch', ['browserSync'], function () {
+    livereload.listen();
+    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch('js/**/*.js',['js']);
+    gulp.watch('**/*.html');
+});
 
-// run gilp watch and voala
+// Default task to be run with `gulp`
+gulp.task('default', ['sass', 'js', 'express', 'watch']);
